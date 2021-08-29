5c2067c68b81adb5f45cef7a610ee7f40ccc1134
Bukhori Inst <58775021+bukhari-inst@users.noreply.github.com>
2021-08-29 21:01:49 +0700

    setting swicth page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { HomePage } from 'pages/HomePage';
+import { auth, db } from 'config/Firebase';
+import LandingPage from 'pages/LandingPage';
+// import { HomePage } from 'pages/HomePage';
 import { SignupPage } from 'pages/SignupPage';
 import { SigninPage } from 'pages/SigninPage';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/homepage" component={HomePage} />
-        <Route exact path="/" component={SignupPage} />
-        <Route exact path="/login" component={SigninPage} />
-      </Switch>
-    </Router>
-  );
+export class App extends Component {
+  state = {
+    currentUser: null,
+  };
+
+  componentDidMount() {
+    // getting current user
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        db.collection('users')
+          .doc(user.uid)
+          .get()
+          .then((snapshot) => {
+            this.setState({
+              currentUser: snapshot.data().Name,
+            });
+          });
+      } else {
+        console.log('user is not signed in to retrive username');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => (
+              <LandingPage currentUser={this.state.currentUser} />
+            )}
+          />
+          {/* <Route exact path="/HomePage" component={HomePage} /> */}
+          <Route exact path="/signup" component={SignupPage} />
+          <Route exact path="/login" component={SigninPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import HomePage from 'pages/HomePage';
 
-export default function LandingPage() {
-  return <div className=""></div>;
-}
+export const LandingPage = ({ currentUser }) => {
+  return (
+    <div>
+      {currentUser && <HomePage />}
+      {!currentUser && <h1>masuk dulu</h1>}
+    </div>
+  );
+};
+
+export default LandingPage;
