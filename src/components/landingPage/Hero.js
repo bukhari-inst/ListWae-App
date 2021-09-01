be51a5275715d3a7e9bc42a65632b9d616277961
Bukhori Inst <58775021+bukhari-inst@users.noreply.github.com>
2021-09-01 20:02:52 +0700

    hero landing page finish

--- src/components/landingPage/Hero.js
+++ src/components/landingPage/Hero.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import Button from 'elements/Button';
+import HeroImage from 'assets/images/hero-landingpage.png';
+import Fade from 'react-reveal/Fade';
 
 export const Hero = () => {
   return (
     <section className="container pt-4 text">
-      <div className="align-item-center justify-content-center">
-        <h1 className="display-4 text-center text-gray-800 mt-xl-5">
-          Kelola daftar kebutuhanmu secara efektif dan efesien
-        </h1>
-        <h4 className="text-center text-gray-600 mt-3 font-weight-normal">
-          Mudah, cepat, efektif dan efesien
-        </h4>
-        <div className="text-center ">
-          <Button
-            className="btn btn-primary mt-lg-5 py-3 px-5"
-            type="link"
-            href="/signup"
-          >
-            Mulai Sekarang
-          </Button>
+      <Fade bottom delay={500}>
+        <div className="align-item-center justify-content-center">
+          <h1 className="display-4 text-center text-gray-800 mt-xl-5">
+            Kelola daftar kebutuhanmu secara efektif dan efesien
+          </h1>
+          <h4 className="text-center text-gray-600 mt-3 font-weight-normal">
+            Mudah, cepat, efektif dan efesien
+          </h4>
+          <div className="text-center ">
+            <Button
+              className="btn btn-primary mt-lg-5 py-3 px-5"
+              type="link"
+              href="/signup"
+            >
+              Mulai Sekarang
+            </Button>
+          </div>
+          <div style={{ marginTop: 72 }}>
+            <img
+              className="rounded mx-auto d-block"
+              src={HeroImage}
+              alt="heroimage"
+              style={{ width: 862, height: 586 }}
+            />
+          </div>
         </div>
-      </div>
+      </Fade>
     </section>
   );
 };
