1c042e2a43859b051d687d644805f5c56beda6d8
Bukhori Inst <58775021+bukhari-inst@users.noreply.github.com>
2021-09-01 20:00:50 +0700

    update contentleft fixed js

--- src/parts/ContentLeft.js
+++ src/parts/ContentLeft.js
@@ -1,7 +1,72 @@
 import React from 'react';
 import Button from 'elements/Button';
-
 export const ContentLeft = () => {
+  function handleClick(e) {
+    e.preventDefault();
+    // console.log(e);
+    const status = e.target.getAttribute('status');
+    var a = e.target.parentElement.parentElement.childNodes;
+    var detail;
+    var short;
+    var n_check;
+    var persen;
+    var e_item;
+    var e_item_cek;
+    var e_progress;
+    var cek_dulu = 0;
+    var n_hiya_hiya;
+    // console.log(cek_dulu);
+    for (var i = 0; i < a.length; i++) {
+      if (a[i].className == 'short-detail') {
+        // console.log(a[i]);
+        short = a[i];
+        e_item = a[i].getElementsByClassName('n-item');
+        e_item_cek = a[i].getElementsByClassName('n-check-item');
+        e_progress = a[i].getElementsByClassName('progress-bar')[0];
+      }
+      if (a[i].className == 'detail-card') {
+        detail = a[i];
+        n_hiya_hiya = a[i].querySelectorAll(
+          'input[type="checkbox"]:checked'
+        ).length;
+        // console.log('okey panjangnya n_hiya_hiya => '+n_hiya_hiya);
+        n_check = a[i].querySelectorAll('input[type="checkbox"]').length;
+        // console.log(n_hiya_hiya);
+        persen = parseInt((n_hiya_hiya / n_check) * 100);
+        // console.log("persen => "+persen);
+      }
+    }
+    // console.log(persen);
+    // console.log('okey panjangnya e_item_cek => '+e_item_cek.length);
+    // console.log(e_item_cek);
+    // console.log(e_progress);
+    console.log(status);
+    if (status == 'detail') {
+      // console.log(short);
+      e.target.children[0].classList.remove('fa-angle-up');
+      e.target.children[0].classList.add('fa-angle-down');
+      for (var i = 0; i < e_item.length; i++) {
+        // console.log(e_item[i]);
+        e_item[i].innerHTML = n_check;
+      }
+      for (var i = 0; i < e_item_cek.length; i++) {
+        e_item_cek[i].innerHTML = n_hiya_hiya;
+      }
+      e_progress.style.width = persen + '%';
+      short.style.display = 'block';
+      detail.style.display = 'none';
+      e.target.setAttribute('status', 'short');
+    } else if (status == 'short') {
+      detail.style.display = 'block';
+      short.style.display = 'none';
+      e.target.children[0].classList.remove('fa-angle-down');
+      e.target.children[0].classList.add('fa-angle-up');
+      e.target.setAttribute('status', 'detail');
+    } else {
+      console.log('status e salah');
+    }
+    // console.log(a);
+  }
   return (
     <div className="row">
       <div className="col-xl-8 col-lg-7">
@@ -9,13 +74,13 @@ export const ContentLeft = () => {
           <div
             id="list-wae-card-header"
             className="
-                        card-header
-                        py-3
-                        d-flex
-                        flex-row
-                        align-items-center
-                        justify-content-between
-                      "
+                          card-header
+                          py-3
+                          d-flex
+                          flex-row
+                          align-items-center
+                          justify-content-between
+                        "
           >
             <h6 className="m-0 font-weight-bold">Earnings Overview</h6>
             <div className="dropdown no-arrow">
@@ -34,10 +99,10 @@ export const ContentLeft = () => {
               </Button>
               <div
                 className="
-                            dropdown-menu dropdown-menu-right
-                            shadow
-                            animated--fade-in
-                          "
+                              dropdown-menu dropdown-menu-right
+                              shadow
+                              animated--fade-in
+                            "
                 aria-labelledby="dropdownMenuLink"
               >
                 <div className="dropdown-header">Dropdown Header:</div>
@@ -80,11 +145,11 @@ export const ContentLeft = () => {
               </p>
               <div
                 className="
-                            d-flex
-                            flex-row
-                            align-items-center align-self-center
-                            progress-list
-                          "
+                              d-flex
+                              flex-row
+                              align-items-center align-self-center
+                              progress-list
+                            "
               >
                 <div className="progress flex-grow-1">
                   <div
@@ -152,7 +217,7 @@ export const ContentLeft = () => {
             <div className="details d-flex">
               <button
                 className="btn flex-grow-1 papa"
-                onClick={'detail(this)'}
+                onClick={handleClick}
                 status="detail"
               >
                 <i className="fa fa-angle-up"></i>
