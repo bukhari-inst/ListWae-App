a58e9af5893bb147209d6b16dfb85368d4ccd905
Bukhori Inst <58775021+bukhari-inst@users.noreply.github.com>
2021-09-01 14:52:36 +0700

    update landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from 'components/landingPage/Header';
 import Fade from 'react-reveal/Fade';
 import Hero from 'components/landingPage/Hero';
+import Content from 'components/landingPage/Content';
+import Footer from 'components/landingPage/Footer';
 
 export const LandingPage = ({ currentUser }) => {
   return (
@@ -10,7 +12,9 @@ export const LandingPage = ({ currentUser }) => {
         <Header />
         <div className="bg-light">
           <Hero />
+          <Content />
         </div>
+        <Footer />
       </Fade>
     </div>
   );
